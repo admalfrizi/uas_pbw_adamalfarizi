bd6222cd8a538976542fadc804088d32b41e84db
Adam Alfarizi Ismail <adam.alfarizi.2002@gmail.com>
2022-01-27 21:42:10 +0700

    Updates

---
+++ precache-manifest.240a2a41a536dc066e7b22cd15c52cb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54665c7e9cac0a3fe8d2faf119469b24",
+    "url": "/uas_pbw_adamalfarizi/index.html"
+  },
+  {
+    "revision": "65555b86dafa763c4f6e",
+    "url": "/uas_pbw_adamalfarizi/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "9730f6d8814db2ba53d8",
+    "url": "/uas_pbw_adamalfarizi/static/css/main.0f44d8e3.chunk.css"
+  },
+  {
+    "revision": "65555b86dafa763c4f6e",
+    "url": "/uas_pbw_adamalfarizi/static/js/2.f6a31ef0.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/uas_pbw_adamalfarizi/static/js/2.f6a31ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4809887aad4c8a90e728",
+    "url": "/uas_pbw_adamalfarizi/static/js/3.bcb14657.chunk.js"
+  },
+  {
+    "revision": "9730f6d8814db2ba53d8",
+    "url": "/uas_pbw_adamalfarizi/static/js/main.037815d3.chunk.js"
+  },
+  {
+    "revision": "a9f281ca8396c11a3b9a",
+    "url": "/uas_pbw_adamalfarizi/static/js/runtime-main.c024d9f6.js"
+  },
+  {
+    "revision": "dc7389947ca397d4e12840c5adc3da89",
+    "url": "/uas_pbw_adamalfarizi/static/media/benefits_bg.dc738994.png"
+  }
+]);
